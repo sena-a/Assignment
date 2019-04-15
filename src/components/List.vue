f602daf96201334080f9443f37b7a72b423cf05a
Ahn sena <serendipity.sena@gmail.com>
2019-04-15 23:27:14 +0900

    4번째 인덱스에 광고 삽입 함수 생성

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,8 @@
 <template>
   <ul>
-    <ListItem v-for="item in list" :key="item.no" v-bind="item"></ListItem>
+    <template v-for="(item, index) in list">
+      <ListItem v-if="(index + 1) % 4 !== 0" :key="item.no" v-bind="item"></ListItem>
+    </template>
   </ul>
 </template>
 
@@ -13,13 +15,34 @@ export default {
   },
   data() {
     return {
-      list: [],
+      postList: [],
       page: 1,
-      bottom: false
+      bottom: false,
+      adsList: [],
+      nextAds: 3,
+      nowAds: 0,
+      adsPage: 1,
+      start: 0,
+      list: []
     };
   },
   methods: {
-    async getList() {
+    async getAds() {
+      const pageIndex = (this.page - 1) % 3;
+      if (pageIndex === 0) {
+        // 광고 가져오기
+        const {
+          data: { list }
+        } = await this.$http.get("http://comento.cafe24.com/ads.php", {
+          params: {
+            page: this.adsPage
+          }
+        });
+        this.adsList = this.adsList.concat(list);
+        ++this.adsPage;
+      }
+    },
+    async getPostList() {
       const {
         data: { list }
       } = await this.$http.get("http://comento.cafe24.com/request.php", {
@@ -27,8 +50,24 @@ export default {
           page: this.page
         }
       });
-      this.list = this.list.concat(list);
+      this.postList = this.postList.concat(list);
       ++this.page;
+      this.getList();
+    },
+    getList() {
+      const newList = [];
+      for (let i = this.start; i < this.postList.length; i++) {
+        if ((i + 1) % 4 === 0) {
+          console.log(i);
+          newList.push(this.adsList[this.nowAds]);
+          ++this.nowAds;
+          this.nextAds += 3;
+        }
+        newList.push(this.postList[i]);
+      }
+      this.start = this.postList.length;
+      this.list = this.list.concat(newList);
+      console.log(this.list);
     },
     isScrollBottom() {
       const isBottom =
@@ -38,18 +77,20 @@ export default {
       return isBottom;
     }
   },
-  watch: {
-    bottom: function(bottom) {
-      if (bottom) {
-        this.getList();
-      }
-    }
-  },
   created() {
     window.addEventListener("scroll", () => {
       this.bottom = this.isScrollBottom();
     });
-    this.getList();
+    this.getAds();
+    this.getPostList();
+  },
+  watch: {
+    bottom: function(bottom) {
+      if (bottom) {
+        this.getAds();
+        this.getPostList();
+      }
+    }
   }
 };
 </script>
