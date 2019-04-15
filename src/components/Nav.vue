c4bfb48fe56a9abd7101d17aa9d566ca870c90b1
Ahn sena <serendipity.sena@gmail.com>
2019-04-15 19:55:47 +0900

    created Navigation for filter&alignment

---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navigation">
+    <button type="button" class="btn btn-outline-primary btn-sm">Fillter</button>
+    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+      <label class="btn btn-outline-primary btn-sm active">
+        <input type="radio" name="options" id="option1" autocomplete="off" checked> 오름차순
+      </label>
+      <label class="btn btn-outline-primary btn-sm">
+        <input type="radio" name="options" id="option2" autocomplete="off"> 내림차순
+      </label>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style scoped>
+</style>
