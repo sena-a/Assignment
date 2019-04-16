9e8e1f8ba6ff60754c94a24b4f456c222922cd25
Ahn sena <serendipity.sena@gmail.com>
2019-04-16 23:12:59 +0900

    CSS: 광고 부분 반응형 적용

--- src/components/Ads.vue
+++ src/components/Ads.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="card mb-3" style="max-width: 540px;">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img :src="'http://comento.cafe24.com/public/images/'+img" class="card-img" alt="...">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">{{title}}</h5>
-          <p class="card-text">{{contents}}</p>
-        </div>
-      </div>
+  <div class="ads-wrapper">
+    <div class="ads-img">
+      <img
+        :src="'http://comento.cafe24.com/public/images/'+img"
+        class="card-img"
+        :alt="title+'광고 이미지'"
+      >
+    </div>
+    <div class="ads-body">
+      <h5 class="ads-title">{{title}}</h5>
+      <p class="ads-text">{{contents}}</p>
     </div>
   </div>
 </template>
@@ -19,5 +19,46 @@ export default {
   props: ["img", "title", "contents"]
 };
 </script>
-<style>
+<style lang="less" scoped>
+.ads {
+  &-wrapper {
+    border: 1px solid rgb(226, 226, 226);
+    border-radius: 4px;
+    @media (min-width: 480px) {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+    }
+  }
+  &-img {
+    @media (min-width: 480px) {
+      display: inline-block;
+      width: 30%;
+    }
+  }
+  &-body {
+    margin: 14px 10px 5px;
+    @media (min-width: 480px) {
+      display: inline-block;
+      width: 70%;
+      margin: 0 10px;
+    }
+  }
+  &-title {
+    font-size: 1rem;
+  }
+  &-text {
+    display: inline-block;
+    width: 100%;
+    white-space: nowrap;
+    overflow: hidden;
+    -o-text-overflow: ellipsis;
+    text-overflow: ellipsis;
+    margin: 0;
+  }
+}
 </style>
+
